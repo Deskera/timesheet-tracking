bd2052749474a33cc2fb736f871908895738c16b
Arnav Bansal <arnavbansal.8232@gmail.com>
2021-09-25 15:29:42 +0530

    navigation styling with active state

--- frontend/src/components/dashboard/common/Sidebar.js
+++ frontend/src/components/dashboard/common/Sidebar.js
@@ -1,41 +1,3 @@
-// import React from "react";
-// import { NavItem, NavLink, Nav, Collapse, Button } from "reactstrap";
-// import { Link } from "react-router-dom";
-
-// function Sidebar() {
-
-//     const [isOpen, setIsOpen] = React.useState(false);
-//     const toggle = () => setIsOpen(!isOpen);
-
-//     return (
-//         <Nav vertical className="mt-4">
-//             <h3 className="text-center" style={{ padding: '20px' }} style={{ color: 'white' }}>Welcome {getUser().user.userDto.firstName}</h3>
-//             <NavItem>
-//                 <NavLink href="/dashboard/overview">Overview</NavLink>
-//             </NavItem>
-//             <NavItem>
-//                 <NavLink href="/dashboard/employee-info">Employee Table</NavLink>
-//             </NavItem>
-//             <NavItem>
-//                 <NavLink onClick={toggle} style={{ cursor: 'pointer' }}>Attendance</NavLink>
-//                 <Collapse isOpen={isOpen}>
-//                     <NavItem style={{ width: "90%", marginLeft: '10%' }}>
-//                         <NavLink href="/dashboard/my-report">My Report</NavLink>
-//                     </NavItem>
-//                     <NavItem style={{ width: "90%", marginLeft: '10%' }}>
-//                         <NavLink href="/dashboard/team-report">Team Report</NavLink>
-//                     </NavItem>
-//                 </Collapse>
-//             </NavItem>
-//             <NavItem>
-//                 <NavLink href="logout">Logout</NavLink>
-//             </NavItem>
-//         </Nav>
-//     );
-// }
-
-// export default Sidebar;
-
 import React, { useState } from 'react';
 import { Button } from 'reactstrap';
 import styled from 'styled-components';
@@ -43,24 +5,6 @@ import { SidebarData } from './SidebarData';
 import SubMenu from './SubMenu';
 import { getUser } from "../../../common/CommonUtils";
 import { useHistory } from 'react-router-dom';
-
-// const Nav = styled.div`
-//   background: #15171c;
-//   height: 80px;
-//   display: flex;
-//   justify-content: flex-start;
-//   align-items: center;
-// `;
-
-// const NavIcon = styled.div`
-//   margin-left: 2rem;
-//   font-size: 2rem;
-//   height: 80px;
-//   display: flex;
-//   justify-content: flex-start;
-//   align-items: center;
-//   cursor: pointer;
-// `;
 
 const SidebarNav = styled.nav`
   background: #15171c;
@@ -94,9 +38,10 @@ const Sidebar = () => {
         <>
             <SidebarNav sidebar={true}>
                 <h3 className="text-center mb-5" style={{ color: 'white' }}>Welcome {getUser().user.userDto.firstName}</h3>
+
                 <SidebarWrap>
                     {SidebarData.map((item, index) => {
-                        return <SubMenu item={item} key={index} />;
+                        return (<SubMenu item={item} key={index} />);
                     })}
                 </SidebarWrap>
 
--- frontend/src/components/dashboard/common/SubMenu.js
+++ frontend/src/components/dashboard/common/SubMenu.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
-const SidebarLink = styled(Link)`
+const SidebarLink = styled(NavLink)`
   display: flex;
   color: #e1e9fc;
   justify-content: space-between;
@@ -13,26 +13,7 @@ const SidebarLink = styled(Link)`
   text-decoration: none;
   font-size: 18px;
 
-  &:hover, &.active  {
-    background: #252831;
-    border-left: 4px solid #632ce4;
-    cursor: pointer;
-  }
-`;
-
-const SidebarDropDown = styled.div`
-  display: flex;
-  color: #e1e9fc;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px;
-  list-style: none;
-  height: 60px;
-  text-decoration: none;
-  font-size: 18px;
-  cursor: pointer;
-
-  &:hover, &.active {
+  &:hover  {
     background: #252831;
     border-left: 4px solid #632ce4;
     cursor: pointer;
@@ -43,65 +24,18 @@ const SidebarLabel = styled.span`
   margin-left: 16px;
 `;
 
-const DropdownLink = styled(Link)`
-  height: 60px;
-  padding-left: 3rem;
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-  color: #f5f5f5;
-  font-size: 18px;
-
-  &:hover, &.active {
-    background: #252831;
-    border-left: 4px solid #632ce4;
-    cursor: pointer;
-  }
-`;
-
 const SubMenu = ({ item }) => {
-    const [subnav, setSubnav] = useState(false);
 
-    const showSubnav = () => setSubnav(!subnav);
-
-    return (
-        <>
-            {item.subNav
-                ?
-                <SidebarDropDown onClick={item.subNav && showSubnav}>
-                    <div>
-                        {item.icon}
-                        <SidebarLabel>{item.title}</SidebarLabel>
-                    </div>
-                    <div>
-                        {item.subNav && subnav
-                            ? item.iconOpened
-                            : item.subNav
-                                ? item.iconClosed
-                                : null}
-                    </div>
-                </SidebarDropDown>
-                :
-                <SidebarLink to={item.path} onClick={item.subNav && showSubnav}>
-                    <div>
-                        {item.icon}
-                        <SidebarLabel>{item.title}</SidebarLabel>
-                    </div>
-                </SidebarLink>
-            }
-
-
-            {subnav &&
-                item.subNav.map((item, index) => {
-                    return (
-                        <DropdownLink to={item.path} key={index}>
-                            {item.icon}
-                            <SidebarLabel>{item.title}</SidebarLabel>
-                        </DropdownLink>
-                    );
-                })}
-        </>
-    );
+  return (
+    <>
+      <SidebarLink to={item.path} style={{ textDecoration: 'none' }} activeStyle={{ background: "#252831", borderLeft: "4px solid #632ce4" }}>
+        <div>
+          {item.icon}
+          <SidebarLabel>{item.title}</SidebarLabel>
+        </div>
+      </SidebarLink>
+    </>
+  );
 };
 
 export default SubMenu;
